fa84c06ff0388f04eec4e82780e14d9f9144fb68
Plai <plai.rt.w@gmail.com>
2022-05-22 02:31:47 +0700

    merge array objects to top level objects

--- swaggerBuilder/swaggerBuilder.ts
+++ swaggerBuilder/swaggerBuilder.ts
@@ -2,7 +2,14 @@ import { stringify } from "https://deno.land/std@0.139.0/encoding/yaml.ts";
 import { file } from "../helper/file.ts";
 
 import { Api } from "../types/models.ts";
-import { Document, NamedSchemaObject, OperationObject, PathsObject } from "../types/OpenAPIV3.ts";
+import {
+  ArraySchemaObject,
+  Document,
+  NamedSchemaObject,
+  OperationObject,
+  PathsObject,
+  SchemaObject,
+} from "../types/OpenAPIV3.ts";
 import { Endpoint } from "./endpoint.ts";
 import { deepCompareSchema, replaceRefs } from "./schemaUtils.ts";
 
@@ -73,9 +80,7 @@ export class SwaggerBuilder {
     }
   }
 
-  renameDuplicatedOpId() {
-    // 1. put `x-pageslug` in every endpoint
-    // 2. find duplicates
+  writeOpIds() {
     const opIds = Object.entries(this.mainSwagger.paths).flatMap(([_, pathitem]) =>
       Object.entries(pathitem!)
         .filter(([method]) => ["get", "post", "put", "patch", "delete"].includes(method))
@@ -83,11 +88,65 @@ export class SwaggerBuilder {
     );
 
     file.writeText("opIds.txt", opIds.join("\n"));
-    // 3. rename duplicates with <pageslug><opname>
+  }
+
+  mergeSecondLevelObjects() {
+    const firstOrder = this.mainSwagger.components?.schemas;
+    if (!firstOrder) return;
+
+    Object.entries(firstOrder).map(([currentname, currentschema], i, all) => {
+      if ((currentschema as SchemaObject).type === "object") {
+        Object.entries((currentschema as SchemaObject).properties!)
+          .filter(([_, prop]) => (prop as SchemaObject).type === "object")
+          .map(([name2, second]) => {
+            // second level of current
+            const match = Object.entries(all).filter(([_, first]) =>
+              deepCompareSchema(first as SchemaObject, second as SchemaObject)
+            );
+
+            if (match.length > 1) console.warn("match more than 1 top level models");
+            if (match.length === 1) {
+              const originName = match[0][0];
+              console.log("Matched!", originName, name2);
+              (
+                (this.mainSwagger.components!.schemas as SchemaObject)!.properties![currentname] as SchemaObject
+              ).properties![name2] = {
+                $ref: `#/components/schemas/${originName}`,
+              };
+            }
+          });
+      }
+    });
+  }
+
+  mergeArrayAndSingleItem() {
+    const topLevel = this.mainSwagger.components?.schemas;
+    Object.entries(topLevel!)
+      .filter(
+        ([_, schema]) =>
+          (schema as SchemaObject).type === "array" &&
+          ((schema as ArraySchemaObject).items as SchemaObject).type === "object"
+      )
+      .forEach(([currentname, currentarray]) => {
+        const matches = Object.entries(topLevel!).filter(([_, toplevelschema]) =>
+          deepCompareSchema(toplevelschema as SchemaObject, (currentarray as ArraySchemaObject).items as SchemaObject)
+        );
+
+        if (matches.length > 1) {
+          console.warn(currentname, "has > 1 match", matches.map(([name]) => name).join(", "));
+        }
+
+        matches.forEach(([matchname]) => {
+          (this.mainSwagger.components!.schemas![currentname] as ArraySchemaObject).items = {
+            $ref: `#/components/schemas/${matchname}`,
+          };
+        });
+      });
   }
 
   async saveMain() {
-    this.renameDuplicatedOpId();
+    this.writeOpIds();
+    this.mergeArrayAndSingleItem();
 
     await file.writeText(
       `swagger.yml`,
