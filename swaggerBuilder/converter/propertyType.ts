cbd51c4c053e51ac74850ae6be4b262367349742
Plai <plai.rt.w@gmail.com>
2022-05-23 01:08:49 +0700

    Refactor + add todos

--- swaggerBuilder/converter/propertyType.ts
+++ swaggerBuilder/converter/propertyType.ts
@@ -1,7 +1,9 @@
 import { SchemaObject } from "../../types/OpenAPIV3.ts";
 
 const intArr = /integer array|array of integers?/i;
-const strArr = /array of (strings?|hash(es)?)|string array/i;
+
+// todo: type "array" defaults to string[] // could be more specified by descrption/name
+const strArr = /array of (strings?|hash(es)?)|string array|^array(\[string\])?$/i;
 const intOrStr = /integer( or |\/)string/i;
 
 export function asPropertyType(_type: string): SchemaObject | undefined {
@@ -20,6 +22,4 @@ export function asPropertyType(_type: string): SchemaObject | undefined {
   if (intArr.test(type)) return { type: "array", items: { type: "integer" } };
   if (type === "datetime") return { type: "string", format: "date-time" };
   if (type === "date") return { type: "string", format: "date" };
-  // todo: type "array" defaults to string[] // could be more specified by descrption/name
-  if (type === "array" || type === "array[string]") return { type: "array", items: { type: "string" } };
 }
